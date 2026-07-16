ab3d34dd2fd88ce80b815a53f125017304a57760
AmirrezaGoleij <analysisanalog06@gmail.com>
2026-07-16 22:30:09 +0330

    Added search and filter for Course and Tag in admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
-from .models import Course, Note, Tag
-
+from .models import Course, Tag, Note
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['title', 'owner', 'created_at']
-    list_filter = ['owner']
-    search_fields = ['title']
+    list_filter = ['created_at']
+    search_fields = ['title', 'description']
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ['title', 'course', 'created_at', 'updated_at']
+    list_display = ['title', 'course', 'updated_at']
     list_filter = ['course', 'tags']
     search_fields = ['title', 'content']
-
-
-admin.site.register(Tag)
